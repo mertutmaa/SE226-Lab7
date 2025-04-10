3a2c4749c9e5eb237f235057836949be2ad7e0bf
MERT UTMA <153324318+mertutmaa@users.noreply.github.com>
2025-04-10 03:36:52 -0700

    Create __init__.py

---
+++ string_package/__init__.py
@@ -0,0 +1,8 @@
+from string_package.manipulate import reverse_string
+from string_package.manipulate import capitalize_words
+from string_package.manipulate import remove_punctuation
+
+from string_package.stats import count_words
+from string_package.stats import count_characters
+from string_package.stats import average_word_length
+
